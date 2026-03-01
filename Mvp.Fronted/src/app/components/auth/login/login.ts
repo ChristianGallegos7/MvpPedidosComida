f26e13479dc468024404fc42da3e702b26fb7110
Satori <Christian@dev.com>
2026-02-28 17:41:45 -0800

    Elimine angular material del proyecto, solo tailwind para estilos

--- Mvp.Fronted/src/app/components/auth/login/login.ts
+++ Mvp.Fronted/src/app/components/auth/login/login.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatCardModule],
+  imports: [],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
-
-}
+export class Login {}
--- Mvp.Fronted/src/app/components/client/navbar/navbar.ts
+++ Mvp.Fronted/src/app/components/client/navbar/navbar.ts
@@ -1,13 +1,9 @@
-import {Component} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'client-navbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
-
-}
+export class Navbar {}
