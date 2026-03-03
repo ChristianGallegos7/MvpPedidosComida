4994a0a0578192f7485cabdc3126d4397146b956
CHRISTIAN A. GALLEGOS YAGUACHI <christian.gallegos@inventar-tech.com>
2026-03-03 14:49:12 -0500

    FUsionado

--- Mvp.Fronted/src/app/components/client/navbar/navbar.ts
+++ Mvp.Fronted/src/app/components/client/navbar/navbar.ts
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-import {Component} from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-@Component({
-  selector: 'client-navbar',
-  imports: [RouterLink],
-=======
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'client-navbar',
   imports: [],
->>>>>>> f26e13479dc468024404fc42da3e702b26fb7110
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
